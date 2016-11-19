5cf1e45ad4b322c95b3b19944e74150476f22eb4
indianajone <mmer555@hotmail.com>
2016-11-20 01:48:47 +0700

    fix typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,5 @@
-declare module strcaser {
-    export function camel_case(str: string): string;
-    export function lower_case(str: string): string;
-    export function snake_case(str: string, diameter?: string): string;
-    export function studle_case(str: string, diameter?: string): string;
-    export function title_case(str: string): string;
-}
-
+export function camel_case(str: string): string;
+export function lower_case(str: string): string;
+export function snake_case(str: string, diameter?: string): string;
+export function studle_case(str: string, diameter?: string): string;
+export function title_case(str: string): string;
