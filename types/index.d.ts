71a190c928fe7ee436fc85757ab265a750dd197c
indianajone <mmer555@hotmail.com>
2016-11-20 00:17:18 +0700

    add types for typescript support

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+declare module strcaser {
+    export function camel_case(str: string): string;
+    export function lower_case(str: string): string;
+    export function snake_case(str: string, diameter?: string): string;
+    export function studle_case(str: string, diameter?: string): string;
+    export function title_case(str: string): string;
+}
+
